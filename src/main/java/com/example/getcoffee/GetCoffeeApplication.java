d24024c52bf24d76b96862db5fe58aa697ffbb8f
Viraj Kaushalye <virajk@codegen.net>
2019-03-18 12:58:34 +0530

    initial commit

---
+++ src/main/java/com/example/getcoffee/GetCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.getcoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetCoffeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetCoffeeApplication.class, args);
+    }
+
+}
